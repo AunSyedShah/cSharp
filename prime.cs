3d536997b7951062b3a6aafe559ee72f664d05e0
Vaishnavi <56784362+vnb09@users.noreply.github.com>
2020-02-04 22:41:33 +0530

    Create prime.cs

---
+++ prime.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static bool PrimeCheck(int n)
+        {
+            for (int i = 2; i < n; i++)
+                if (n % i == 0)
+                    return false;
+            return true;
+        }
+        
+        public static void Main()
+        {
+            
+            Console.WriteLine("To check a number is prime or not :");
+            Console.WriteLine("--------------------------------------------------");
+            Console.WriteLine("Input a number : ");
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            if (PrimeCheck(a))
+                Console.WriteLine($"{a} is a prime number");
+            else
+                Console.WriteLine($"{a} is not a prime number");
+            Console.ReadLine();
+        }
+     }
+}
