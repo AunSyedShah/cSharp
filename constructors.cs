55b44999170d6562e4ae7f2fe33a2a29349a391c
HarshitPathak009 <56058977+HarshitPathak009@users.noreply.github.com>
2020-02-05 18:37:17 +0530

    Add files via upload

---
+++ constructors.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class constructors
+{
+	int a;
+	public constructors() //default constructor
+	{
+		Console.WriteLine("The default constructor was called");
+		a = 100;
+	}
+	public constructors(int c) //parameterized constructor
+	{
+		Console.WriteLine("The parametized constructor was called");
+		a =c;
+	}
+	static void Main(string[] ar)
+	{
+		constructors obj = new constructors();//Instantiation
+		Console.WriteLine("The value of a is "+obj.a);
+		constructors obj1 = new constructors(150);//Instantiation
+		Console.WriteLine("The value of a is "+obj1.a);
+		constructors obj2 = new constructors(200);//Instantiation
+		Console.WriteLine("The value of a is " + obj2.a+"\nPrevious value was"+obj1.a);
+	}
+}
