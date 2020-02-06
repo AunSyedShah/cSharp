83349ec07463887bc004561f133ee47b46f7b7db
HarshitPathak009 <56058977+HarshitPathak009@users.noreply.github.com>
2020-02-06 21:09:35 +0530

    Add files via upload
    Method overloading in c#. Methods with the same name are differentiated on the basis of number, order and type of parameters.

---
+++ overload.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public class overload
+{
+    public int add(int a, int b=100)//1
+    {
+        return (a + b);
+    }
+    public int add(int a, int b, int c)//2
+    {
+        return (a + b + c);
+    }
+    public int add(double a, int b)//3
+    {
+        return Convert.ToInt32(a + b);
+    }
+    public double add(int a, double b)//4
+    {
+        return (a + b);
+    }
+    public double add(double a, double b)//5
+    {
+        return (a + b);
+    }
+    public static void Main(string []ar)
+    {
+        overload obj = new overload();
+        Console.WriteLine(obj.add(10,20,30));//call to 2
+        Console.WriteLine(obj.add(10.5,90));//call to 3
+        Console.WriteLine(obj.add(10.5,20.9));//call to 5
+        Console.WriteLine(obj.add(9,10.5));//call to 4
+        Console.WriteLine(obj.add(10,20));//call to 1
+        Console.WriteLine(obj.add(10));//call to 1
+    }
+}
