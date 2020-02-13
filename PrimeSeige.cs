569c2646be540e1056592ed15ff4b1a1a8e8a02b
HarshitPathak009 <56058977+HarshitPathak009@users.noreply.github.com>
2020-02-13 17:46:11 +0530

    PrimeSeive.cs
    To find the first n prime numbers using  the Sieve of Eratosthenes method.

---
+++ PrimeSeige.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+public class PrimeSeige
+	{
+		public static void Main(string[] ar)
+		{
+			int n;
+			bool flg;
+			Console.WriteLine("Enter a number");
+			n = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine($"The first {n} prime nummbers are ->");
+			ArrayList prime = new ArrayList();
+			int i = 2;
+		    if(n>0)
+			prime.Add(i);
+		    n -= 1;
+		    i = 3;
+			while (n > 0)
+			{
+				flg = true;
+				foreach (var j in prime)
+				{
+					if (i % Convert.ToInt32(j) == 0)
+						flg = false;
+				}
+				if (flg == true)
+				{
+					prime.Add(i);
+					n--;
+				}
+				i+=2;
+			}
+			foreach(var j in prime)
+			Console.WriteLine(j);
+		}
+	}
